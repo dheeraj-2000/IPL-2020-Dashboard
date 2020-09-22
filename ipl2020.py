1416829649ad28e1642664e3a159951775e9c5fc
dheeraj2000 <dheeraj12000@gmail.com>
2020-09-23 00:18:53 +0530

    updated repo

--- ipl2020.py
+++ ipl2020.py
@@ -1,3 +1,7 @@
+# -----------------------------------------------------------
+#                   (C) Dheeraj Chaudhary
+# -----------------------------------------------------------
+
 import streamlit as st
 import pandas as pd
 import numpy as np
